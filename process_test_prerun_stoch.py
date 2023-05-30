a3e974bfeab3d2a414f2e3df4da3f830abd2f077
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2023-05-30 15:55:38 +0200

    initial commit

---
+++ process_test_prerun_stoch.py
@@ -0,0 +1,56 @@
+
+import sys
+import numpy as np
+import scipy.io
+import matplotlib.pyplot as plt
+import copy
+import math
+from scipy.fftpack import fft, ifft
+import h5py
+import matplotlib.animation as animation
+
+
+plt.close('all')
+
+solStringName = 'test_prerun_stoch'
+# solStringName = 'testRuns_debug_1'
+
+file = h5py.File('../solution/' + solStringName + '.h5' , 'r+')
+u    = file['U'][:]
+v    = file['V'][:]
+k    = file['TKE'][:]
+T    = file['Temp'][:]
+z    = file['z'][:] # meters
+dt   = file['dt'][:] # seconds
+SimStart = int(file['SimStart' ][:])
+SimEnd = int(file['SimEnd' ][:])
+file.close()
+
+# create grid for plotting
+nt = SimEnd - SimStart
+t    = np.linspace(0, nt * dt / (3600), nt+1) # in hours
+X, Y = np.meshgrid(t, z)
+
+
+plt.pcolor(X, Y, k)
+plt.xlabel('time [h]')
+plt.ylabel('z [h]')
+plt.colorbar()
+plt.title('TKE')
+
+fig, ax = plt.subplots()
+
+ax.set_xlabel('u[m/s]')
+ax.set_ylabel('z[m]')
+line, = ax.plot(u[:, 0], z)
+
+def animate(i):
+    print(i)
+    line.set_xdata(u[:, i])  # update the data.
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, frames=179, interval=1, blit=True, repeat=False)
+
+ani.save('u_stoch.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
